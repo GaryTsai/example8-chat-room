5fb2bace7bdc0d3bf16825e9d51c993a3ea7701c
Gary Tsai <Gary_Tsai@PFT.com>
2019-07-11 16:27:06 +0800

    Updates

---
+++ precache-manifest.16e0ec1fcbb06ad65e5c14a77e8b3b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c51c90983e36af1f66e523e5ff8d40",
+    "url": "/example8-chat-room/index.html"
+  },
+  {
+    "revision": "ee16016f11656d9614d1",
+    "url": "/example8-chat-room/static/css/main.4d130de4.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/example8-chat-room/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "ee16016f11656d9614d1",
+    "url": "/example8-chat-room/static/js/main.312355d8.chunk.js"
+  },
+  {
+    "revision": "7847e8b19f2af9a463ca",
+    "url": "/example8-chat-room/static/js/runtime~main.bdb9e9e9.js"
+  }
+]);
